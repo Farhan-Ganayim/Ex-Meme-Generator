97008aab1ba38b6866d7a7c8abc9d9fe1d10264a
Farhan-Ganayim <f.ganayim@gmail.com>
2024-12-22 14:36:54 +0200

    Add meme.servisce & implement getMeme()

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -3,20 +3,24 @@
 let gElCanvas
 let gCtx
 
-renderMeme()
-
-function renderMeme() {
+function onInit() {
 
     gElCanvas = document.querySelector('.meme-canvas')
     gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
 
+function renderMeme() {
+
+    const meme = getMeme()
     const img = new Image()
-    img.src = 'imgs/3.jpg'
+    img.src = `imgs/${meme.selectedImgId}.jpg`
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-
-        gCtx.font = '30px Arial';
-        gCtx.fillStyle = 'red';
-        gCtx.fillText('First Meme', 50, 50)
+        meme.lines.forEach(line => {
+            gCtx.font = '30px Arial';
+            gCtx.fillStyle = 'red';
+            gCtx.fillText(line.txt, 50, 50)
+        })
     }
 }
---
+++ js/meme.service.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var gMeme = {
+    selectedImgId: 3,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'First Meme',
+            size: 20,
+            color: 'black'
+        }
+    ]
+}
+
+function getMeme(){
+    return gMeme
+}
+
+
